9cb59fd3a326cf4ca9cc05169aeabe40befde330
Z <z@mysoft.tech>
2020-07-15 15:08:42 +0800

    Update.

--- UserScripts/hao123-printable-calendar.js
+++ UserScripts/hao123-printable-calendar.js
@@ -1,19 +1,30 @@
 // ==UserScript==
 // @name         Printable Calendar for Hao123
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Make Hao123 Calendar Printable
 // @author       Ziming Li
 // @match        https://www.hao123.com/rili/
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    document.getElementById('erjiV2Header').remove();
-    document.querySelector('.h_calendar_select').remove();
-    document.querySelector('.h_calendar_right').remove();
-    document.querySelector('.today').classList.remove('today');
-    document.querySelector('.h_calendar_exp').remove();
-    document.querySelector('.sites-top').remove();
-    document.querySelector('.c-footer').remove();
+    'use strict';
+
+    var a = document.createElement('a');
+    a.innerText = '可打印版本';
+    a.className = 'item';
+    a.onclick = function() {
+        document.getElementById('erjiV2Header').remove();
+        document.querySelector('.h_calendar_select').remove();
+        document.querySelector('.h_calendar_right').remove();
+        document.querySelector('.h_calendar_exp').remove();
+        document.querySelector('.sites-top').remove();
+        document.querySelector('.c-footer').remove();
+        document.querySelector('.today').classList.remove('today');
+    };
+
+    var bottomNav = document.querySelector('div.bottom-nav');
+    bottomNav.insertBefore(a, bottomNav.firstChild);
 })();
