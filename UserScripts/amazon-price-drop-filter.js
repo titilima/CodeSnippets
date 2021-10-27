b33432dad827a6eaef2dfa20624fa636628204c1
Z <z@mysoft.tech>
2021-10-27 17:33:46 +0800

    Update.

--- UserScripts/amazon-price-drop-filter.js
+++ UserScripts/amazon-price-drop-filter.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         亚马逊降价商品过滤
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.7
 // @description  亚马逊降价商品自动过滤
 // @author       titilima
+// @match        https://www.amazon.cn/hz/wishlist/ls/*
 // @match        https://www.amazon.cn/gp/registry/wishlist?*
 // @run-at       document-end
 // @grant        none
@@ -15,27 +16,48 @@
     var button = document.createElement('button');
     button.innerText = '自动化过滤降价商品';
     button.onclick = function() {
+        button.disabled = true;
+
+        var count = 0;
+        var cleanup = function(left) {
+            var items = document.querySelectorAll('.g-item-sortable');
+            for (var i = 0; i < items.length - left; ++i) {
+                var e = items[i];
+                if (e.innerHTML.indexOf('不可用') != -1) {
+                    continue;
+                }
+                var priceDrop = e.querySelector('.itemPriceDrop');
+                if (!priceDrop) {
+                    ++count;
+                    e.remove();
+                    button.innerText = '已过滤 ' + count + ' 个商品';
+                }
+            }
+        };
+
         var lastHeight = document.documentElement.scrollHeight;
-        window.scrollBy(0, 2000);
+        window.scrollTo(0, lastHeight);
+
+        var retry = 0;
         var scrollId = window.setInterval(function() {
             var currentHeight = document.documentElement.scrollHeight;
             if (lastHeight == currentHeight) {
+                if (++retry < 5) {
+                    return;
+                }
+                cleanup(0);
                 clearInterval(scrollId);
-                var items = document.querySelectorAll('.g-item-sortable');
-                items.forEach(function (e) {
-                    var priceDrop = e.querySelector('.itemPriceDrop');
-                    if (!priceDrop) {
-                        e.remove();
-                    }
-                });
                 return;
             } else {
                 lastHeight = currentHeight;
-                window.scrollBy(0, 2000);
+                retry = 0;
+                cleanup(5);
+                window.scrollTo(0, currentHeight);
             }
-        }, 2000);
+        }, 3000);
     };
 
     var searchForm = document.getElementById('wl-item-search');
+    console.log(searchForm);
     searchForm.parentNode.appendChild(button);
 })();
